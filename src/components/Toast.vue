b21bf156855c32e2939e01fb7a3a1c5cb3ff8583
yuanyuan1103 <pijhs70401@gmail.com>
2022-06-29 22:56:31 +0800

    update toast style

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -19,7 +19,7 @@ export default {
     const toastEl = this.$refs.toast;
     //此吐司顯示5s
     const toast = new Toast(toastEl, {
-      delay: 1000
+      delay: 5000
     });
     toast.show();
   }
--- src/components/ToastMessages.vue
+++ src/components/ToastMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast-container position-fixed bottom-0 end-0 p-3 translate-middle" style="z-index: 1050">
+  <div class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 1050">
     <Toast v-for="(msg, key) in messages" :key="key" :msg="msg" />
   </div>
 </template>
