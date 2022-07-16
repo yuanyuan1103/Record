9a8761cdaad4c5a621e83523b2c9c3ed6744214a
yuanyuan1103 <pijhs70401@gmail.com>
2022-07-17 02:58:31 +0800

    update footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,6 +13,16 @@
           <p>紀錄回憶的聲音</p>
         </div>
       </div>
+      <div class="footerAbout col-lg-4 col-sm-8 lh-sm p-3">
+        <h2 class="fs-2 mb-2 text-muted fw-bolder">聯絡方式</h2>
+        <p class="pt-2 space"><i class="bi bi-geo-alt-fill pe-1"></i>地球上某個角落</p>
+        <div class="d-flex">
+          <i class="bi bi-clock-fill pe-1"></i>
+          <p class="space">週一至週五 13:30 - 21:50<br />週六 10:00 - 21:50<br />週日 10:00 - 18:00</p>
+        </div>
+        <p class="space"><i class="bi bi-envelope-fill pe-1"></i>pijhs70401@gmail.com</p>
+        <p class="space"><i class="bi bi-telephone-fill pe-1"></i>(09)-8765-4321</p>
+      </div>
       <div class="contact col-lg-3 col-sm-8 lh-sm p-3">
         <h2 class="fs-2 mb-2 text-muted fw-bolder">關於本站</h2>
         <div class="d-flex flex-column space">
@@ -30,16 +40,6 @@
             ><i class="bi bi-caret-right-fill"></i>Github</a
           >
         </div>
-      </div>
-      <div class="footerAbout col-lg-4 col-sm-8 lh-sm p-3">
-        <h2 class="fs-2 mb-2 text-muted fw-bolder">聯絡方式</h2>
-        <p class="pt-2 space"><i class="bi bi-geo-alt-fill pe-1"></i>地球上某個角落</p>
-        <div class="d-flex">
-          <i class="bi bi-clock-fill pe-1"></i>
-          <p class="space">週一至週五 13:30 - 21:50<br />週六 10:00 - 21:50<br />週日 10:00 - 18:00</p>
-        </div>
-        <p class="space"><i class="bi bi-envelope-fill pe-1"></i>pijhs70401@gmail.com</p>
-        <p class="space"><i class="bi bi-telephone-fill pe-1"></i>(09)-8765-4321</p>
       </div>
       <div class="space text-center">
         <p>僅個人練習使用．不做商業用途 | <router-link to="/login" class="iconFont">管理員登入</router-link></p>
