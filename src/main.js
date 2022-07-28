669729467a98009d8e67c3504065a3c62bc6ca67
yuanyuan1103 <pijhs70401@gmail.com>
2022-07-28 12:59:15 +0800

    整理註解

--- src/main.js
+++ src/main.js
@@ -9,34 +9,30 @@ import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import { currency, date } from './methods/filters';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
-// 匯入 vee-validate 相關規則
 import { required, email, min } from '@vee-validate/rules';
-// 匯入多國語系的功能
 import { localize, setLocale } from '@vee-validate/i18n';
-// 匯入繁體中文語系檔案
 import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 import AllRules from '@vee-validate/rules';
 
-// 定義驗證規則
 defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
 Object.keys(AllRules).forEach((rule) => {
   defineRule(rule, AllRules[rule]);
 });
-// 設定 vee-validate 全域規則
+
 configure({
-  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
-  validateOnInput: true // 當輸入任何內容直接進行驗證
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true
 });
-// 設定預設語系
+
 setLocale('zh_TW');
 const app = createApp(App);
 app.config.globalProperties.$filters = {
   date,
   currency
 };
-// 註冊 vee-validate 三個全域元件
+
 app.component('Form', Form);
 app.component('Field', Field);
 app.component('ErrorMessage', ErrorMessage);
