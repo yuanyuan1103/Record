9188460f61dcd75aeb04f93ea26e5ec4e29e1ee2
yuanyuan1103 <pijhs70401@gmail.com>
2022-08-09 18:43:08 +0800

    刪除多於判斷

--- src/components/FrontNavbar.vue
+++ src/components/FrontNavbar.vue
@@ -36,13 +36,13 @@
         <router-link to="/favorite" class="nav-link"
           ><i class="bi bi-heart"></i
           ><span class="position-absolute badge rounded-pill bg-danger textS" v-if="favorite.length"
-            >{{ favorite.length ? favorite.length : '' }}<span class="visually-hidden">unread messages</span></span
+            >{{ favorite.length }}<span class="visually-hidden">unread messages</span></span
           ></router-link
         >
         <router-link to="/cart" class="position-relative nav-link">
           <i class="bi bi-cart3"></i>
           <span class="position-absolute badge rounded-pill bg-danger textS" v-if="cart"
-            >{{ cart ? cart : '' }}<span class="visually-hidden">unread messages</span></span
+            >{{ cart }}<span class="visually-hidden">unread messages</span></span
           >
         </router-link>
       </div>
